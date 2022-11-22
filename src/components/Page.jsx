1b2a170baf72ddbc40b27470f1e2a2c3bca3750c
Alex Nico <a.nicolaidis@outlook.com>
2022-11-22 17:25:31 +1100

    Added Button

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import parse from "html-react-parser";
 import "../Page.css";
+import Button from "@mui/material/Button";
 
 const Page = () => {
   const [editor, setEditor] = useState(true);
@@ -28,10 +29,17 @@ const Page = () => {
 
   return (
     <div>
-      <button onClick={log}>Log editor content</button>
-      <button onClick={_convertToHTML}>Convert to html </button>
-      <button onClick={_convertToText}>Convert back to editor</button>
-      <h1>This is the page title</h1>
+      <div className="page-buttons">
+        <Button variant="outlined" size="small" onClick={log}>
+          Log editor content
+        </Button>
+        <Button variant="outlined" size="small" onClick={_convertToHTML}>
+          Convert to html
+        </Button>
+        <Button variant="outlined" size="small" onClick={_convertToText}>
+          Convert back to editor
+        </Button>
+      </div>
       <div className={editor ? "hide-text" : ""}>{parse(content)}</div>
       <div className={editor ? "" : "hide-editor"}>
         <Editor
@@ -43,15 +51,32 @@ const Page = () => {
             height: 500,
             menubar: true,
             plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table paste code help wordcount image",
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "print",
+              "preview",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "paste",
+              "code",
+              "help",
+              "wordcount",
             ],
             toolbar:
               "undo redo | formatselect | " +
               "bold italic backcolor | alignleft aligncenter " +
               "alignright alignjustify | bullist numlist outdent indent | " +
-              "removeformat | help | image",
+              "removeformat | help | image | table",
             image_title: true,
 
             content_style:
