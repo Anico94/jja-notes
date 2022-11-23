612cb14e640cf592e7a39c5f2547e73ed782f688
Joshuanluo <joshua.chuan.luo@gmail.com>
2022-11-23 16:34:29 +1100

    add avatar and change avatar

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,23 @@
-import { auth, logOut } from "../firebase-config";
+import { auth, logOut, storage } from "../firebase-config";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import SearchAppBar from "./AppBarTest";
 import MainMenu from "./Notebooks";
+import { useState, useEffect } from "react";
+import { ref, getDownloadURL, listAll } from "firebase/storage";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+import { Card } from "@mui/material";
 
 const Profile = () => {
   const navigate = useNavigate();
   //   const [currentUser, setCurrentUser] = useState();
   const [user] = useAuthState(auth);
-
+  const avatarDeafult =
+    "https://firebasestorage.googleapis.com/v0/b/jja-notes.appspot.com/o/images%2Favatar%2FavatarDefault.png?alt=media&token=2beea97a-6301-4a96-afed-fc4f69db3b4a";
+  const [avatarURL, setAvatarURL] = useState(avatarDeafult);
   const handleLogout = async () => {
     const res = await logOut();
     console.log(res);
@@ -16,6 +25,18 @@ const Profile = () => {
       navigate("/");
     }
   };
+  const avatarRef = ref(storage, `images/avatar/${user.uid}`);
+
+  useEffect(() => {
+    getDownloadURL(avatarRef)
+      .then((url) => {
+        console.log("avatarUrl: ", url);
+        setAvatarURL(url);
+      })
+      .catch(() => {
+        setAvatarURL(avatarDeafult);
+      });
+  }, []);
 
   const currentUserInfo = (user) => {
     if (!user) {
@@ -24,14 +45,29 @@ const Profile = () => {
     }
     return (
       <div>
-        <h1>Welcome Back {user.displayName}!</h1>
-        <p>Display Name: {user.displayName}</p>
-        <p>
-          Email: {user.email} ({user.emailVerified ? "verified" : "unverified"})
-        </p>
-        <p>Last Logged In: {Date(Date.parse(user.metadata.lastSignInTime))}</p>
-        <h3>TODO: add popup that allows 1. edit display name 2. avatar</h3>
-        <Link to="/profile/:uid/edit">Edit</Link>
+        <div>
+          <h1>Welcome Back {user.displayName}!</h1>
+          <Stack spacing={3}>
+            <Card sx={{ padding: 10 }}>
+              <Stack spacing={4}>
+                <Avatar src={avatarURL} sx={{ width: 100, height: 100 }} />
+                <p>Name: {user.displayName}</p>
+                <p>
+                  Email: {user.email} (
+                  {user.emailVerified ? "verified" : "unverified"})
+                </p>
+                <p>
+                  Last Logged In:{" "}
+                  {Date(Date.parse(user.metadata.lastSignInTime))}
+                </p>
+              </Stack>
+            </Card>
+
+            <Link to="/profile/:uid/edit">
+              <Button variant="contained">Edit</Button>
+            </Link>
+          </Stack>
+        </div>
       </div>
     );
   };
--- src/components/ProfileEdit.jsx
+++ src/components/ProfileEdit.jsx
@@ -5,14 +5,21 @@ import SearchAppBar from "./AppBarTest";
 import MainMenu from "./Notebooks";
 import { useEffect, useState } from "react";
 import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
-import { v4 } from "uuid";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { Box, Card, TextField } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 
 const ProfileEdit = () => {
   const navigate = useNavigate();
   //   const [currentUser, setCurrentUser] = useState();
   const [user] = useAuthState(auth);
   const [imageUpload, setImageUpload] = useState(null);
+  const [imageDisplay, setImageDisplay] = useState(null);
   const [imageList, setImageList] = useState([]);
+  const [userName, setUserName] = useState(user.displayName);
+
   const handleLogout = async () => {
     const res = await logOut();
     console.log(res);
@@ -21,28 +28,39 @@ const ProfileEdit = () => {
     }
   };
 
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  };
+
   // image upload
+  const handleImgChange = (e) => {
+    setImageUpload(e.target.files[0]);
+    setImageDisplay(URL.createObjectURL(e.target.files[0]));
+  };
   const uploadImage = () => {
     if (imageUpload === null) return;
-    const imageRef = ref(storage, `images/avatar/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then(() => {
-      console.log("image upload");
+    const imageRef = ref(storage, `images/avatar/${user.uid}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      console.log("image has uploaded");
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList([url]);
+      });
     });
   };
 
   //image show
-  const imageListRef = ref(storage, "images/avatar/");
+  const imageListRef = ref(storage, `images/avatar/${user.uid}`);
   useEffect(() => {
-    listAll(imageListRef).then((response) => {
-      //   console.log("this is response.item: ", response.items);
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          //   console.log("this is url: ", url);
-          setImageList((prev) => [...prev, url]);
-          //   console.log(imageList);
-        });
+    getDownloadURL(imageListRef)
+      .then((url) => {
+        setImageList([url]);
+      })
+      .catch((error) => {
+        console.log(error);
+        setImageList([
+          "https://firebasestorage.googleapis.com/v0/b/jja-notes.appspot.com/o/images%2Favatar%2FavatarDefault.png?alt=media&token=2beea97a-6301-4a96-afed-fc4f69db3b4a",
+        ]);
       });
-    });
   }, []);
 
   const currentUserInfo = (user) => {
@@ -54,28 +72,40 @@ const ProfileEdit = () => {
       <div>
         <div>
           <h1>Welcome Back {user.displayName}!</h1>
-          <p>Display Name: {user.uid}</p>
-          <p>
-            Email: {user.email} (
-            {user.emailVerified ? "verified" : "unverified"})
-          </p>
-          <p>
-            Last Logged In: {Date(Date.parse(user.metadata.lastSignInTime))}
-          </p>
-          <h3>TODO: add popup that allows 1. edit display name 2. avatar</h3>
+          <Stack spacing={3}>
+            <Card sx={{ padding: 10 }}>
+              <Stack spacing={4}>
+                <Avatar src={imageList[0]} sx={{ width: 100, height: 100 }} />
+                <p>Name: {user.displayName}</p>
+                <input
+                  required
+                  placeholder={user.displayName}
+                  onChange={handleChange}
+                />
+
+                <p>
+                  Email: {user.email} (
+                  {user.emailVerified ? "verified" : "unverified"})
+                </p>
+                {/* <input placeholder={user.email} /> */}
+              </Stack>
+            </Card>
+          </Stack>
         </div>
         <div>
+          <img src={imageDisplay} />
+          <input
+            type="file"
+            required
+            accept="image/*"
+            onChange={handleImgChange}
+          />
           {imageList.map((url) => {
             return <img src={url} key={url} />;
           })}
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-              setImageUpload(e.target.files[0]);
-            }}
-          />
-          <button onClick={uploadImage}>upload image</button>
+          <Button variant="contained" onClick={uploadImage}>
+            upload image
+          </Button>
         </div>
       </div>
     );
